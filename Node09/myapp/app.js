95ec0f09c3ee2d8c6e8674fb4673cceddb1fe989
HideXiong <HideXiong@qq.com>
2019-12-24 21:20:29 +0800

    ‘template

--- Node09/myapp/app.js
+++ Node09/myapp/app.js
@@ -9,7 +9,7 @@ app.get('/',function (req,res) {
 
 app.get('/baby',function(req,res){
 	fs.readFile('../../Node02/image/timg.jpg',function(err,data){
-		err ? console.log(err): res.end(data);//注意这里因为不是文本不能用send，send会直接下载。
-	});
+		err ? console.log(err) : res.end(data);//注意这里因为不是文本不能用send，send会直接下载。
+	});	
 })
 app.listen(3000);
--- Node09/myapp/template.js
+++ Node09/myapp/template.js
@@ -4,6 +4,8 @@ var app = express();
 
 app.engine('html', require('express-art-template'))
 
+app.use('/public/',express.static('public'));
+
 app.get('/',function(req,res){
 	res.render('index.html');
 })
